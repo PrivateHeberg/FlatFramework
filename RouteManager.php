8c54b3d5388daa84fb2230c85d4008ec9be5a590
PrivateHeberg <contact@privateheberg.com>
2016-09-28 04:45:56 +0200

    Ajoute le systéme de resultat au router car cela manque sur alto

---
+++ RouteManager.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Marc Moreau
+ * Date: 28/09/2016
+ * Time: 04:44
+ */
+class RouteManager extends Altorouter
+{
+
+    public function result() {
+        // match current request url
+        $match = $this->match();
+        $error = false;
+        if (count($match) != 0) {
+            try {
+                $fx_data = explode('#', $match['target']);
+                if (isset($fx_data[0]) and isset($fx_data[1])) {
+                    $obj_name = $fx_data[0];
+                    $obj_method = $fx_data[1];
+                    $obj = new $obj_name();
+                    if (DEBUG == false) ob_clean();
+                    call_user_func_array(array($obj, $obj_method), $match['params']);
+                } else
+                    $error = true;
+            } catch (Exception $e) {
+                $error = true;
+                if (DEBUG) {
+                    echo $e;
+                }
+            }
+        } else {
+            $error = true;
+        }
+        if ($error) {
+            // no route was matched
+            //header('Location:  /404');
+        }
+    }
+
+}
